510285873a5e30b239b16724342c0062da7c5ee2
your Ashish <hackz.talks@gmail.com>
2026-01-20 23:46:06 +0530

    Add files via upload

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+import "./globals.css"
+
+export const metadata = {
+  title: "I'm Sorry Ritu...! ❤️‍🩹",
+  description: "I apologize...😿",
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
