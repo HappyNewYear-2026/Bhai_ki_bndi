ae8e5060dd41ddc18f2dff78dbc0f7218968ca84
your Ashish <hackz.talks@gmail.com>
2026-01-25 19:28:55 +0530

    HugPage.jsx

--- src/components/HugPage.jsx
+++ src/components/HugPage.jsx
@@ -138,7 +138,7 @@ export default function HugPage({ setCurrentPage }) {
           }}
         />
         <Heart size={24} className="text-pink-200 relative z-10" />
-        <span className="relative z-10">✨</span>
+        <span className="relative z-10">.</span>
       </motion.button>
 
       <AnimatePresence>
